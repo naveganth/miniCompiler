37a3a113e6ed756744da0000b78641199255cf84
Gabriel-1201 <pessoal@gabrielataide.com>
2025-06-09 16:41:01 -0300

    Lê e organiza o código

--- src/MiniCompiler.java
+++ src/MiniCompiler.java
@@ -1,16 +1,46 @@
-// import java.util.ArrayList;
-// import java.util.List;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class MiniCompiler {
 
-public static void main(String[] args) {
-    SymbolTable symbolTable = new SymbolTable();
-    IntermediateCodeGenerator codeGen = new IntermediateCodeGenerator();
+public static void main(String[] args) throws IOException {
+    String fonte = LerFonte(Path.of("./src/MiniCalc"));
 
-System.out.println("variables");
-// TODO: Declarar variaveis na tabela
+    SymbolTable symbolTable = new SymbolTable();
+    // IntermediateCodeGenerator codeGen = new IntermediateCodeGenerator();
+
+    System.out.println("variables");
+// Declarar variaveis na tabela
 // TODO: Analisar expresses e gerar codigo intermediario
 // TODO: Inicializar variaveis conforme uso
 // TODO: Exibir tabela e codigo gerado
     }
+
+    public static String LerFonte(Path local) throws IOException {
+        String fonteCrua = Files.readString(local);
+        System.out.println("Código fonte: " + fonteCrua);
+
+
+        String buffer = fonteCrua.replaceAll("\\n", " ").replaceAll("\\s{2,}", " ");
+        System.out.println("Código fonte bunito: " + buffer);
+        return buffer;
+    }
+
+    public class Token {
+        String tipo;
+        String valor;
+
+        public Token(String tipo, String valor) {
+            this.tipo = tipo;
+            this.valor = valor;
+        }
+    }
+
+    public static void CriarTokens() {
+
+    }
 }
+
