288f51501cacde0af0c77675350ce883ae787b75
Naveganth <lucas.navegant@gmail.com>
2025-06-09 16:43:48 -0300

    Testando e removendo o arquivo calc do src e adicionando gitignore. Adicionando pasta do prof de compiladores

--- src/IntermediateCodeGenerator.java
+++ src/IntermediateCodeGenerator.java
@@ -1,19 +1,58 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class IntermediateCodeGenerator {
- public List < String > generate ( String assignmentLine ) {
-// TODO : implementar parsing basico da expresso e gerar digo de 3 endereos
- return new ArrayList < >() ;
- }
 
- public List < String > optimize ( List < String > code ) {
-// TODO : aplicar otimizações simples ( ex : + 0 , * 1)
- return code ;
- }
+    private int tempCounter = 0;
+    private SymbolTable symbolTable;
 
- public void printCode ( List < String > code ) {
-// TODO : exibir codigo linha por linha
- }
- }    
+    public IntermediateCodeGenerator(SymbolTable symbolTable) {
+        this.symbolTable = symbolTable;
+    }
 
+    public List<String> generate(String expression) {
+        List<String> code = new ArrayList<>();
+        Stack<String> stack = new Stack<>();
+
+        // Divide a expressão em tokens
+        String[] tokens = expression.split("(?<=[-+*/()])|(?=[-+*/()])");
+
+        for (String token : tokens) {
+            token = token.trim();
+            if (token.isEmpty()) {
+                continue;
+            }
+
+            if (isOperator(token)) {
+                if (stack.size() >= 2) {
+                    String op2 = stack.pop();
+                    String op1 = stack.pop();
+                    String temp = "t" + (++tempCounter);
+                    code.add(temp + " = " + op1 + " " + token + " " + op2);
+                    stack.push(temp);
+                }
+            } else {
+                stack.push(token);
+            }
+        }
+
+        return code;
+    }
+
+    private boolean isOperator(String token) {
+        return token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/");
+    }
+
+    public List<String> optimize(List<String> code) {
+        // TODO: aplicar otimizações simples (ex: + 0, * 1)
+        return code;
+    }
+
+    // Exibe o código linha por linha
+    public void printCode(List<String> code) {
+        for (String line : code) {
+            System.out.println(line);
+        }
+    }
+}
